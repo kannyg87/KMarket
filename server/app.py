eb42c95cd1947bdb8d1c49d4cef312be9f008405
Kanny <kanny.ghafour87@gmail.com>
2024-06-01 13:44:34 -0500

    create table user

--- server/app.py
+++ server/app.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python3
 
-# Standard library imports
-
 # Remote library imports
-from flask import request
+from flask import request, current_app
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
-
+from models import User
 
 # Views go here!
-
 @app.route('/')
 def index():
-    return '<h1>Project Server</h1>'
-
+    host = request.headers.get("Host")
+    appname = current_app.name
+    return f'''<h1>The host for this page is {host}</h1>
+                <h2>The name of this application is {appname}</h2>'''
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
--- server/models.py
+++ server/models.py
@@ -1,6 +1,16 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+# models.py
 
+from sqlalchemy_serializer import SerializerMixin
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    confPassword = db.Column(db.String, nullable=False)
+    is_select = db.Column(db.Boolean, default=False)
+    phone_number = db.Column(db.String, nullable=True)
